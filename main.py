844c454963155ea20811c097130f7f2cdb56165b
Yuvraj <yuvrajnaruka40@gmail.com>
2026-07-11 22:19:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from contextlib import asynccontextmanager
+from typing import Annotated
 
+from fastapi import Depends, FastAPI, HTTPException, Request, status
+from fastapi.exception_handlers import (
+    http_exception_handler,
+    request_validation_exception_handler,
+)
+from fastapi.exceptions import RequestValidationError
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
+from starlette.exceptions import HTTPException as StarletteHTTPException
